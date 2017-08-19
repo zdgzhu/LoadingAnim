237a6f2ab794f641a5f59267e75c7e3ca1435185
zhudonggang <zhudonggang>
2017-08-19 23:40:54 +0800

    开始写WiFi加载的动画

---
+++ app/src/main/java/com/loadinganim/customView/WifiLoadingAnim.java
@@ -0,0 +1,24 @@
+package com.loadinganim.customView;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/8/19.
+ */
+
+public class WifiLoadingAnim extends View{
+    public WifiLoadingAnim(Context context) {
+        super(context);
+    }
+
+    public WifiLoadingAnim(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WifiLoadingAnim(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
